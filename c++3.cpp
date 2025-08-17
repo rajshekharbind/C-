b3c184741230d888f80ec83324e621027f04435d
Your Name <raju.rajsekhar123@gmail.com>
2025-08-18 00:41:27 +0530

    update the code

---
+++ c++3.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+/*int power(int a,int b){
+    int ans=1;
+    for(int i=1;i<=b;i++){
+    ans=ans*a;
+    }
+    return ans;
+}*/
+
+int main(){
+   /* int a,b;
+    cin>>a>>b;
+    int ans=power(a,b);
+
+cout<<"answeris:"<<ans<<endl;
+return 0;*/
+int arr[10]={5,8,0,8,65,6};
+int n=10;
+cout<<"printing the array"<<endl;
+
+for(int i=0;i<6;i++){
+cout<<arr[i]<<endl;
+}
+
+}
